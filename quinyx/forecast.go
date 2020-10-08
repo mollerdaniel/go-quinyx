74429522740da6692fbcb33d4ea33001cb570b11
Daniel Möller <daniel.moller@quinyx.com>
2020-10-08 10:46:30 +0200

    feat: added last endpoints

--- quinyx/forecast.go
+++ quinyx/forecast.go
@@ -20,6 +20,22 @@ type DataProviderInputList struct {
 	DataProviderInputs []DataProvider `json:"requests"`
 }
 
+// PredictedDataInputList PredictedDataInputList
+type PredictedDataInputList struct {
+	ForecastPredictions []ForecastPrediction `json:"requests"`
+}
+
+// ForecastPrediction ForecastPrediction
+type ForecastPrediction struct {
+	ExternalForecastVariableID      string    `json:"externalForecastVariableId,omitempty"`
+	ExternalForecastConfigurationID string    `json:"externalForecastConfigurationId,omitempty"`
+	ExternalUnitID                  string    `json:"externalUnitId,omitempty"`
+	ExternalSectionID               string    `json:"externalSectionId,omitempty"`
+	RunIdentifier                   string    `json:"runIdentifier,omitempty"`
+	RunTimestamp                    Timestamp `json:"runTimestamp,omitempty"`
+	Payloads                        []Payload `json:"forecastDataPayload"`
+}
+
 // DataProvider is the input object to feed Quinyx forecast
 type DataProvider struct {
 	ExternalForecastVariableID string    `json:"externalForecastVariableId,omitempty"`
@@ -34,6 +50,13 @@ type Payload struct {
 	Timestamp Timestamp `json:"timestamp,omitempty"`
 }
 
+// AggregatedPayload is the aggregated data object
+type AggregatedPayload struct {
+	Data      float64   `json:"data,omitempty"`
+	EndTime   Timestamp `json:"endTime,omitempty"`
+	StartTime Timestamp `json:"startTime,omitempty"`
+}
+
 // RequestOptions is the options object for querying forecast
 type RequestOptions struct {
 	StartTime         time.Time `url:"startTime,omitempty"`
@@ -41,6 +64,54 @@ type RequestOptions struct {
 	ExternalSectionID *string   `url:"externalSectionId,omitempty"`
 	ExternalUnitID    *string   `url:"externalUnitId,omitempty"`
 }
+
+// EditCalculatedOptions is the options object for editing calculated forecast
+type EditCalculatedOptions struct {
+	ExternalSectionID *string `url:"externalSectionId,omitempty"`
+	ExternalUnitID    *string `url:"externalUnitId,omitempty"`
+}
+
+// CalculatedForecast is the calculated forecast generated in Quinyx Forecast
+type CalculatedForecast struct {
+	DataPayload                     []CalculatedPayload `json:"dataPayload,omitempty"`
+	ExternalForecastConfigurationID *string             `json:"externalForecastConfigurationId,omitempty"`
+	ExternalSectionID               *string             `json:"externalSectionId,omitempty"`
+	ExternalUnitID                  *string             `json:"externalUnitId,omitempty"`
+}
+
+// CalculatedPayload is the payload data object for calculated data
+type CalculatedPayload struct {
+	Data       float64   `json:"data,omitempty"`
+	EditedData float64   `json:"editedData,omitempty"`
+	StartTime  Timestamp `json:"startTime,omitempty"`
+	EndTime    Timestamp `json:"endTime,omitempty"`
+}
+
+// EditCalculatedRequest is the object used to edit calculated forecast
+type EditCalculatedRequest struct {
+	RepetitionSetup        bool      `json:"repetitionSetup,omitempty"`
+	StartTime              Timestamp `json:"startTime,omitempty"`
+	EndTime                Timestamp `json:"endTime,omitempty"`
+	PercentageModification float64   `json:"percentageModification,omitempty"`
+	NewValueForPeriod      float64   `json:"newValueForPeriod,omitempty"`
+	WeekDays               []Weekday `json:"weekdays,omitempty"`
+	RepetitionEndDate      Timestamp `json:"repetitionEndDate,omitempty"`
+	WeekPattern            int32     `json:"weekPattern,omitempty"`
+}
+
+// Weekday defines a weekday
+type Weekday string
+
+// TagTypes
+const (
+	Monday    Weekday = "0"
+	Tuesday           = "1"
+	Wednesday         = "2"
+	Thursday          = "3"
+	Friday            = "4"
+	Saturday          = "5"
+	Sunday            = "6"
+)
 
 // UploadActualData sends raw datapoints to Quinyx Forecast API
 func (s *ForecastService) UploadActualData(ctx context.Context, appendData bool, dil *DataProviderInputList) (*Response, error) {
@@ -92,6 +163,16 @@ func (g *RequestOptions) hasRequiredFields() bool {
 	return true
 }
 
+func (g *EditCalculatedOptions) hasRequiredFields() bool {
+	if g == nil {
+		return false
+	}
+	if g.ExternalUnitID == nil {
+		return false
+	}
+	return true
+}
+
 // GetActualData gets the actual data previously uploaded for the given forecast variable.
 // The range between these two dates can not exceed 120 days.
 func (s *ForecastService) GetActualData(ctx context.Context, externalForecastVariableID string, requestoptions *RequestOptions) ([]*DataProvider, *Response, error) {
@@ -134,6 +215,168 @@ func (s *ForecastService) DeleteActualData(ctx context.Context, externalForecast
 		return nil, err
 	}
 	req.URL.RawQuery = v.Encode()
+	resp, err := s.client.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+// GetActualDataStream gets the actual data previously uploaded for the given forecast variable.
+// The range between these two dates can not exceed 120 days.
+func (s *ForecastService) GetActualDataStream(ctx context.Context, externalForecastVariableID string, requestoptions *RequestOptions) ([]*DataProvider, *Response, error) {
+	u := fmt.Sprintf("/forecasts/forecast-variables/%v/actual-data-stream", externalForecastVariableID)
+	if !requestoptions.hasRequiredFields() {
+		return nil, nil, fmt.Errorf("Required fields in the Options not provided, see docs")
+	}
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	v, err := query.Values(requestoptions)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.URL.RawQuery = v.Encode()
+	var dp []*DataProvider
+	resp, err := s.client.Do(ctx, req, &dp)
+	if err != nil {
+		return nil, resp, err
+	}
+	return dp, resp, nil
+}
+
+// GetAggregatedData gets the aggregated data for the given forecast variable.
+// The range between these two dates can not exceed 120 days.
+func (s *ForecastService) GetAggregatedData(ctx context.Context, externalForecastVariableID string, requestoptions *RequestOptions) ([]*AggregatedPayload, *Response, error) {
+	u := fmt.Sprintf("/forecasts/forecast-variables/%v/aggregated-data", externalForecastVariableID)
+	if !requestoptions.hasRequiredFields() {
+		return nil, nil, fmt.Errorf("Required fields in the Options not provided, see docs")
+	}
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	v, err := query.Values(requestoptions)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.URL.RawQuery = v.Encode()
+	var dp []*AggregatedPayload
+	resp, err := s.client.Do(ctx, req, &dp)
+	if err != nil {
+		return nil, resp, err
+	}
+	return dp, resp, nil
+}
+
+// GetCalculatedForecast gets the calculated forecast for the given forecast variable.
+// The range between these two dates can not exceed 120 days.
+func (s *ForecastService) GetCalculatedForecast(ctx context.Context, externalForecastVariableID string, requestoptions *RequestOptions) ([]*CalculatedForecast, *Response, error) {
+	u := fmt.Sprintf("/forecasts/forecast-variables/%v/calculated-forecast", externalForecastVariableID)
+	if !requestoptions.hasRequiredFields() {
+		return nil, nil, fmt.Errorf("Required fields in the Options not provided, see docs")
+	}
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	v, err := query.Values(requestoptions)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.URL.RawQuery = v.Encode()
+	var cf []*CalculatedForecast
+	resp, err := s.client.Do(ctx, req, &cf)
+	if err != nil {
+		return nil, resp, err
+	}
+	return cf, resp, nil
+}
+
+// EditCalculatedForecast changes the calculated forecast.
+func (s *ForecastService) EditCalculatedForecast(ctx context.Context, externalForecastVariableID string, externalForecastConfigurationID string, requestoptions *EditCalculatedOptions, modrequest *EditCalculatedRequest) (*Response, error) {
+	u := fmt.Sprintf("/forecasts/forecast-variables/%v/forecast-configurations/%v/edit-forecast", externalForecastVariableID, externalForecastConfigurationID)
+	if !requestoptions.hasRequiredFields() {
+		return nil, fmt.Errorf("Required fields in the Options not provided, see docs")
+	}
+	req, err := s.client.NewRequest("POST", u, modrequest)
+	if err != nil {
+		return nil, err
+	}
+	v, err := query.Values(requestoptions)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.RawQuery = v.Encode()
+	resp, err := s.client.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+// GetForecastData gets the uploaded forecast data for the given forecast variable.
+// The range between these two dates can not exceed 120 days.
+func (s *ForecastService) GetForecastData(ctx context.Context, externalForecastVariableID string, requestoptions *RequestOptions) ([]*DataProvider, *Response, error) {
+	u := fmt.Sprintf("/forecasts/forecast-variables/%v/forecast-data", externalForecastVariableID)
+	if !requestoptions.hasRequiredFields() {
+		return nil, nil, fmt.Errorf("Required fields in the Options not provided, see docs")
+	}
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	v, err := query.Values(requestoptions)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.URL.RawQuery = v.Encode()
+	var dp []*DataProvider
+	resp, err := s.client.Do(ctx, req, &dp)
+	if err != nil {
+		return nil, resp, err
+	}
+	return dp, resp, nil
+}
+
+// DeleteForecastData deletes the previously uploaded forecast data for the the given forecast variable.
+// The startTime and endTime must be at the start of hour and the range between these two dates can not exceed 120 days.
+func (s *ForecastService) DeleteForecastData(ctx context.Context, externalForecastVariableID string, requestoptions *RequestOptions) (*Response, error) {
+	u := fmt.Sprintf("/forecasts/forecast-variables/%v/forecast-data", externalForecastVariableID)
+	if !requestoptions.hasRequiredFields() {
+		return nil, fmt.Errorf("Required fields in the Options not provided, see docs")
+	}
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	v, err := query.Values(requestoptions)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.RawQuery = v.Encode()
+	resp, err := s.client.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+// UploadPredictedData is the Operation used to upload generated prediction data.
+// Resolution of datapoints must match expected resolution of variable.
+// The total amount of data rows must not exceed 366
+func (s *ForecastService) UploadPredictedData(ctx context.Context, inlist *PredictedDataInputList) (*Response, error) {
+	u := fmt.Sprintf("/forecasts/predicted-data")
+	if inlist != nil {
+		if len(inlist.ForecastPredictions) > maxRowsPerCall {
+			return nil, fmt.Errorf("The total amount of data rows must not exceed 366 in a single call")
+		}
+	}
+	req, err := s.client.NewRequest("POST", u, inlist)
+	if err != nil {
+		return nil, err
+	}
 	resp, err := s.client.Do(ctx, req, nil)
 	if err != nil {
 		return resp, err
--- quinyx/forecast_test.go
+++ quinyx/forecast_test.go
@@ -125,3 +125,304 @@ func TestGetActualData(t *testing.T) {
 	options.ExternalSectionID = String("foo")
 	client.Forecast.GetActualData(context.Background(), "a", options)
 }
+
+func TestGetActualDataStream(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &RequestOptions{
+		StartTime:      time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC),
+		EndTime:        time.Date(2019, time.October, 13, 07, 20, 50, 520000000, time.UTC),
+		ExternalUnitID: String("d"),
+	}
+
+	want := []*DataProvider{
+		{
+			ExternalForecastVariableID: "b",
+			ExternalSectionID:          "c",
+			ExternalUnitID:             "d",
+			DataPayload: []Payload{
+				{
+					Data:      123,
+					Timestamp: Timestamp{time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+				},
+			},
+		},
+	}
+	mux.HandleFunc("/forecasts/forecast-variables/a/actual-data-stream", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "GET")
+		assert.Equal(t, "2019-10-13T07:20:50Z", r.URL.Query().Get("endTime"))
+		assert.Equal(t, "2019-10-12T07:20:50Z", r.URL.Query().Get("startTime"))
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+		json, err := json.Marshal(&want)
+		assert.NilError(t, err)
+		fmt.Fprint(w, string(json))
+	})
+
+	dps, _, err := client.Forecast.GetActualDataStream(context.Background(), "a", options)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, want, dps)
+	_, _, err = client.Forecast.GetActualDataStream(context.Background(), "a", &RequestOptions{})
+	assert.ErrorContains(t, err, "Required fields in the Options")
+
+	mux.HandleFunc("/forecasts/forecast-variables/b/actual-data-stream", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "foo", r.URL.Query().Get("externalSectionID"))
+	})
+	options.ExternalSectionID = String("foo")
+	client.Forecast.GetActualData(context.Background(), "a", options)
+}
+
+func TestGetAggregatedData(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &RequestOptions{
+		StartTime:      time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC),
+		EndTime:        time.Date(2019, time.October, 13, 07, 20, 50, 520000000, time.UTC),
+		ExternalUnitID: String("d"),
+	}
+
+	want := []*AggregatedPayload{
+		{
+			Data:      1234,
+			StartTime: Timestamp{time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+			EndTime:   Timestamp{time.Date(2019, time.October, 12, 07, 21, 50, 520000000, time.UTC)},
+		},
+	}
+	mux.HandleFunc("/forecasts/forecast-variables/a/aggregated-data", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "GET")
+		assert.Equal(t, "2019-10-13T07:20:50Z", r.URL.Query().Get("endTime"))
+		assert.Equal(t, "2019-10-12T07:20:50Z", r.URL.Query().Get("startTime"))
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+		json, err := json.Marshal(&want)
+		assert.NilError(t, err)
+		fmt.Fprint(w, string(json))
+	})
+
+	dps, _, err := client.Forecast.GetAggregatedData(context.Background(), "a", options)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, want, dps)
+	_, _, err = client.Forecast.GetAggregatedData(context.Background(), "a", &RequestOptions{})
+	assert.ErrorContains(t, err, "Required fields in the Options")
+
+	mux.HandleFunc("/forecasts/forecast-variables/b/aggregated-data", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "foo", r.URL.Query().Get("externalSectionID"))
+	})
+	options.ExternalSectionID = String("foo")
+	client.Forecast.GetActualData(context.Background(), "a", options)
+}
+
+func TestGetCalculatedForecast(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &RequestOptions{
+		StartTime:      time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC),
+		EndTime:        time.Date(2019, time.October, 13, 07, 20, 50, 520000000, time.UTC),
+		ExternalUnitID: String("d"),
+	}
+
+	want := []*CalculatedForecast{
+		{
+			DataPayload: []CalculatedPayload{
+				{
+					Data:       1234,
+					EditedData: 4321,
+					StartTime:  Timestamp{time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+					EndTime:    Timestamp{time.Date(2019, time.October, 12, 07, 21, 50, 520000000, time.UTC)},
+				},
+			},
+			ExternalForecastConfigurationID: String("b"),
+			ExternalSectionID:               String("c"),
+			ExternalUnitID:                  String("d"),
+		},
+	}
+	mux.HandleFunc("/forecasts/forecast-variables/a/calculated-forecast", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "GET")
+		assert.Equal(t, "2019-10-13T07:20:50Z", r.URL.Query().Get("endTime"))
+		assert.Equal(t, "2019-10-12T07:20:50Z", r.URL.Query().Get("startTime"))
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+		json, err := json.Marshal(&want)
+		assert.NilError(t, err)
+		fmt.Fprint(w, string(json))
+	})
+
+	dps, _, err := client.Forecast.GetCalculatedForecast(context.Background(), "a", options)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, want, dps)
+	_, _, err = client.Forecast.GetCalculatedForecast(context.Background(), "a", &RequestOptions{})
+	assert.ErrorContains(t, err, "Required fields in the Options")
+
+	mux.HandleFunc("/forecasts/forecast-variables/b/calculated-forecast", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "foo", r.URL.Query().Get("externalSectionID"))
+	})
+	options.ExternalSectionID = String("foo")
+	client.Forecast.GetActualData(context.Background(), "a", options)
+}
+
+func TestEditCalculatedForecast(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &EditCalculatedOptions{
+		ExternalUnitID: String("d"),
+	}
+
+	want := EditCalculatedRequest{
+		RepetitionSetup:        true,
+		StartTime:              Timestamp{time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+		EndTime:                Timestamp{time.Date(2019, time.October, 12, 07, 21, 50, 520000000, time.UTC)},
+		PercentageModification: 1234,
+		NewValueForPeriod:      4321,
+		WeekDays:               []Weekday{Wednesday},
+		RepetitionEndDate:      Timestamp{time.Date(2019, time.October, 12, 07, 22, 50, 520000000, time.UTC)},
+		WeekPattern:            1,
+	}
+	mux.HandleFunc("/forecasts/forecast-variables/a/forecast-configurations/b/edit-forecast", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "POST")
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+		json, err := json.Marshal(&want)
+		assert.NilError(t, err)
+		assert.Equal(t, `{"repetitionSetup":true,"startTime":"2019-10-12T07:20:50.52Z","endTime":"2019-10-12T07:21:50.52Z","percentageModification":1234,"newValueForPeriod":4321,"weekdays":["2"],"repetitionEndDate":"2019-10-12T07:22:50.52Z","weekPattern":1}`, string(json))
+	})
+
+	_, err := client.Forecast.EditCalculatedForecast(context.Background(), "a", "b", options, &want)
+	assert.NilError(t, err)
+}
+
+func TestGetForecastData(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &RequestOptions{
+		StartTime:      time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC),
+		EndTime:        time.Date(2019, time.October, 13, 07, 20, 50, 520000000, time.UTC),
+		ExternalUnitID: String("d"),
+	}
+	want := []*DataProvider{
+		{
+			ExternalForecastVariableID: "a",
+			ExternalSectionID:          "b",
+			ExternalUnitID:             "c",
+			DataPayload: []Payload{
+				{
+					Data:      1234.4,
+					Timestamp: Timestamp{time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+				},
+			},
+		},
+	}
+
+	mux.HandleFunc("/forecasts/forecast-variables/a/forecast-data", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "GET")
+		assert.Equal(t, "2019-10-13T07:20:50Z", r.URL.Query().Get("endTime"))
+		assert.Equal(t, "2019-10-12T07:20:50Z", r.URL.Query().Get("startTime"))
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+		json, err := json.Marshal(&want)
+		assert.NilError(t, err)
+		fmt.Fprint(w, string(json))
+	})
+
+	dps, _, err := client.Forecast.GetForecastData(context.Background(), "a", options)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, want, dps)
+	_, _, err = client.Forecast.GetForecastData(context.Background(), "a", &RequestOptions{})
+	assert.ErrorContains(t, err, "Required fields in the Options")
+
+	mux.HandleFunc("/forecasts/forecast-variables/b/forecast-data", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "foo", r.URL.Query().Get("externalSectionID"))
+	})
+	options.ExternalSectionID = String("foo")
+	client.Forecast.GetActualData(context.Background(), "a", options)
+}
+
+func TestDeleteForecast(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &RequestOptions{
+		StartTime:      time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC),
+		EndTime:        time.Date(2019, time.October, 13, 07, 20, 50, 520000000, time.UTC),
+		ExternalUnitID: String("d"),
+	}
+
+	mux.HandleFunc("/forecasts/forecast-variables/a/forecast-data", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "DELETE")
+		assert.Equal(t, "2019-10-13T07:20:50Z", r.URL.Query().Get("endTime"))
+		assert.Equal(t, "2019-10-12T07:20:50Z", r.URL.Query().Get("startTime"))
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+	})
+
+	_, err := client.Forecast.DeleteForecastData(context.Background(), "a", options)
+	assert.NilError(t, err)
+	_, err = client.Forecast.DeleteForecastData(context.Background(), "a", &RequestOptions{})
+	assert.ErrorContains(t, err, "Required fields in the Options")
+
+	mux.HandleFunc("/forecasts/forecast-variables/b/forecast-data", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "foo", r.URL.Query().Get("externalSectionID"))
+	})
+	options.ExternalSectionID = String("foo")
+	client.Forecast.GetActualData(context.Background(), "a", options)
+}
+
+func TestDeleteActualData(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	options := &RequestOptions{
+		StartTime:      time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC),
+		EndTime:        time.Date(2019, time.October, 13, 07, 20, 50, 520000000, time.UTC),
+		ExternalUnitID: String("d"),
+	}
+
+	mux.HandleFunc("/forecasts/forecast-variables/a/actual-data", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+		assert.Equal(t, "2019-10-13T07:20:50Z", r.URL.Query().Get("endTime"))
+		assert.Equal(t, "2019-10-12T07:20:50Z", r.URL.Query().Get("startTime"))
+		assert.Equal(t, "d", r.URL.Query().Get("externalUnitId"))
+	})
+
+	_, err := client.Forecast.DeleteActualData(context.Background(), "a", options)
+	assert.NilError(t, err)
+	_, err = client.Forecast.DeleteActualData(context.Background(), "a", &RequestOptions{})
+	assert.ErrorContains(t, err, "Required fields in the Options")
+
+	mux.HandleFunc("/forecasts/forecast-variables/b/actual-data", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "foo", r.URL.Query().Get("externalSectionID"))
+	})
+	options.ExternalSectionID = String("foo")
+	client.Forecast.DeleteActualData(context.Background(), "a", options)
+}
+
+func TestUploadPredictedData(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	want := &PredictedDataInputList{
+		ForecastPredictions: []ForecastPrediction{
+			{
+				ExternalForecastVariableID:      "a",
+				ExternalForecastConfigurationID: "d",
+				ExternalSectionID:               "b",
+				ExternalUnitID:                  "c",
+				RunIdentifier:                   "e",
+				RunTimestamp:                    Timestamp{time.Date(2020, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+				Payloads: []Payload{
+					{
+						Data:      123,
+						Timestamp: Timestamp{time.Date(2019, time.October, 12, 07, 20, 50, 520000000, time.UTC)},
+					},
+				},
+			},
+		},
+	}
+	mux.HandleFunc("/forecasts/predicted-data", func(w http.ResponseWriter, r *http.Request) {
+
+		testMethod(t, r, "POST")
+		body, err := ioutil.ReadAll(r.Body)
+		assert.Equal(t, `{"requests":[{"externalForecastVariableId":"a","externalForecastConfigurationId":"d","externalUnitId":"c","externalSectionId":"b","runIdentifier":"e","runTimestamp":"2020-10-12T07:20:50.52Z","forecastDataPayload":[{"data":123,"timestamp":"2019-10-12T07:20:50.52Z"}]}]}
+`, string(body))
+		assert.NilError(t, err)
+	})
+
+	_, err := client.Forecast.UploadPredictedData(context.Background(), want)
+	assert.NilError(t, err)
+}
